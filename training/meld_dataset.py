180a0210f642c1bdab3525e8b3effb36fd58a983
alexwox <alexander.woxstrom@gmail.com>
2025-03-11 02:36:04 +0100

    Dataset implementation completed

--- training/meld_dataset.py
+++ training/meld_dataset.py
@@ -1,7 +1,12 @@
 from torch.utils.data import Dataset
 import pandas as pd
+import numpy as np
 from transformers import AutoTokenizer
 import os
+import cv2
+import torch
+import subprocess
+import torchaudio
 
 class MELDDataset(Dataset):
     def __init__(self, csv_path, video_dir):
@@ -26,20 +31,133 @@ class MELDDataset(Dataset):
             'positive': 2
         }
         
+    def _load_video_frames(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+        try: 
+            if not cap.isOpened():
+                raise ValueError(f"Cannot open video {video_path}")
+            ret, frame = cap.read()
+            if not ret or frame is None:
+                raise ValueError(f"Cannot open video {video_path}")
+            
+            #Reset index to avoid frame skio
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            
+            while (len(frames) < 30) and cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                frame = cv2.resize(frame, (224,224))
+                frame = frame / 255.0
+                frames.append(frame)
+            
+        except Exception as e:
+            raise ValueError(f"Video error: {str(e)}") from e
+        finally:
+            cap.release()
+            
+        if(len(frames) == 0):
+            raise ValueError("No frames could be extracted")
+        
+        if(len(frames) <30):
+            frames += [np.zeros_like(frames[0])] * (30 - len(frames))
+        else:
+            frames = frames[:30]
+        
+        return torch.FloatTensor(np.array(frames)).permute(0, 3, 1, 2)
+            
+    def _extract_audio_features(self, video_path):
+        audio_path = video_path.replace('.mp4', '.wav')
+        
+        try:
+            subprocess.run([
+                'ffmpeg',
+                '-i', video_path,
+                '-vn',
+                '-acodec', 'pcm_s16le',
+                '-ar', '16000',
+                '-ac', '1',
+                audio_path
+            ], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            
+            waveform, sample_rate = torchaudio.load(audio_path)
+            
+            if sample_rate != 16000:
+                resampler = torchaudio.transforms.Resample(sample_rate, 16000)
+                waveform = resampler(waveform)
+                
+            mel_spectogram = torchaudio.transforms.MelSpectrogram(
+                sample_rate=16000,
+                n_mels=64,
+                n_fft = 1024,
+                hop_length = 512,
+            )
+            
+            mel_spec = mel_spectogram(waveform)
+            mel_spec = (mel_spec - mel_spec.mean()) / mel_spec.std()
+            
+            if mel_spec.size(2) < 300:
+               padding = 300 - mel_spec.size(2) 
+               mel_spec = torch.nn.functional.pad(mel_spec, (0, padding))
+            else:
+                mel_spec = mel_spec[:,:,:300]
+                
+            return mel_spec
+        
+        except subprocess.CalledProcessError as e:
+            raise ValueError(f"Audio extraction failed {str(e)}") from e
+        except Exception as e:
+            raise ValueError(f"Audio error {str(e)}") from e
+        finally:
+            if os.path.exists(audio_path):
+                os.remove(audio_path)
+            
+    
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
+        if isinstance(idx, torch.Tensor):
+            idx = idx.item()
         row = self.data.iloc[idx]
-        video_filename = f"""dia{row['Dialogue_ID']}_utt{row['Utterance_ID']}.mp4"""
+        try: 
+            video_filename = f"""dia{row['Dialogue_ID']}_utt{row['Utterance_ID']}.mp4"""
+            
+            path = os.path.join(self.video_dir, video_filename)
+            video_path_exists = os.path.exists(path)
+            
+            if not video_path_exists:
+                raise FileNotFoundError(f"No video found for filename: {path}")
+            
+            text_inputs = self.tokenizer(row['Utterance'], 
+                                        padding='max_length', 
+                                        truncation=True,
+                                        max_length=128,
+                                        return_tensors='pt')
+            
+            
+            video_frames = self._load_video_frames(path)
+            audio_features = self._extract_audio_features(path)
+            
+            emotion_label = self.emotion_map[row['Emotion'].lower()]
+            sentiment_label = self.sentiment_map[row['Sentiment'].lower()]
+            
+            return {
+                'text_inputs': {
+                    'input_ids': text_inputs['input_ids'].squeeze(),
+                    'attention_mask': text_inputs['attention_mask'].squeeze()
+                },
+                'video_frames':video_frames,
+                'audio_features': audio_features,
+                'emotion_label': torch.tensor(emotion_label),
+                'sentiment_label': torch.tensor(sentiment_label)
+            }
+        except Exception as e:
+            print(f"Error processing {path} {str(e)}")
+            return None
         
-        path = os.path.join(self.video_dir, video_filename)
-        video_path = os.path.exists(path)
-        
-        if not video_path:
-            raise FileNotFoundError(f"No video found for filename: {path}")
-        
-        print("File found")
         
             
         
